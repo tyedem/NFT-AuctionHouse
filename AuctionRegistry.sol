2ede2c8046ecbfcb8a916c6b6da4194c826086b5
tyedem <90783116+tyedem@users.noreply.github.com>
2022-04-29 16:03:27 -0400

    Add registration fee to AuctionRegistry contract

--- AuctionRegistry.sol
+++ AuctionRegistry.sol
@@ -46,21 +46,37 @@ contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable, MaticUsdMumbaiOra
     Counters.Counter private _tokenIdCounter;
     address contractAddress;
 
+    address payable auctionOwner;
+    uint256 registrationPrice = 0.01 ether;
+
     constructor(address auctionAddress) ERC721("AuctionRegistry", "AUT") {
         contractAddress = auctionAddress;
     }
+
+    struct NFT {
+        address owner;
+        string name;
+        string creator;
+        string uri;
+        uint256 tokenId;
+    }
+
+    mapping(uint256 => NFT) public AuctionCollection;
 
     function registerNFT(
         address owner,
         string memory name,
         string memory creator,
         string memory uri
-    ) public onlyOwner {
+    ) public payable onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
 
         _tokenIdCounter.increment();
         _safeMint(owner, tokenId);
         _setTokenURI(tokenId, uri);
+
+        require(msg.value == registrationPrice, "Price must be equal to listing price");
+        payable(msg.sender);
 
     }
 
