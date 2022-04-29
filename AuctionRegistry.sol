fa8780a1060e01ba26bced4ce294f0e7aa73400c
tyedem <90783116+tyedem@users.noreply.github.com>
2022-04-28 21:20:58 -0400

    Rename NFT2.sol to AuctionRegistry.sol

--- AuctionRegistry.sol
+++ AuctionRegistry.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
