62891166f8ff573c51b621229b7e77ffc0633c48
tyedem <90783116+tyedem@users.noreply.github.com>
2022-04-29 13:45:38 -0400

    Inherit oracle contract in AuctionRegistry contract

--- AuctionRegistry.sol
+++ AuctionRegistry.sol
@@ -7,7 +7,41 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable {
+
+contract MaticUsdMumbaiOracle {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Polygon Testnet (Mumbai)
+     * Aggregator: MATIC/USD
+     * Address: 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (
+        uint80 roundID, 
+        int price,
+        uint startedAt,
+        uint timeStamp,
+        uint80 answeredInRound
+    ) {
+        (
+            roundID, 
+            price,
+            startedAt,
+            timeStamp,
+            answeredInRound
+        ) = priceFeed.latestRoundData();
+    }   
+}
+
+contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable, MaticUsdMumbaiOracle {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
     address contractAddress;
@@ -44,38 +78,4 @@ contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable {
     {
         return super.tokenURI(tokenId);
     }
-    
-}
-
-contract MaticUsdMumbaiOracle {
-
-    AggregatorV3Interface internal priceFeed;
-
-    /**
-     * Network: Polygon Testnet (Mumbai)
-     * Aggregator: MATIC/USD
-     * Address: 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
-     */
-    constructor() {
-        priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
-    }
-
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice() public view returns (
-        uint80 roundID, 
-        int price,
-        uint startedAt,
-        uint timeStamp,
-        uint80 answeredInRound
-    ) {
-        (
-            roundID, 
-            price,
-            startedAt,
-            timeStamp,
-            answeredInRound
-        ) = priceFeed.latestRoundData();
-    }   
 }
