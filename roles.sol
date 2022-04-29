89b3747e33198340bb3bb1c0104ab65585036eb2
tyedem <90783116+tyedem@users.noreply.github.com>
2022-04-29 16:40:58 -0400

    Explore Access control in roles.sol + clean AuctionRegistry.sol

--- roles.sol
+++ roles.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.4;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-
 
 contract MaticUsdMumbaiOracle {
 
@@ -41,43 +40,34 @@ contract MaticUsdMumbaiOracle {
     }   
 }
 
-contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable, MaticUsdMumbaiOracle {
+contract AuctionRegistry is ERC721, ERC721URIStorage, AccessControl {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
     address contractAddress;
 
-    address payable auctionOwner;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    Counters.Counter private _tokenIdCounter;
+
     uint256 registrationPrice = 0.01 ether;
 
     constructor(address auctionAddress) ERC721("AuctionRegistry", "AUT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
         contractAddress = auctionAddress;
     }
-
-    struct NFT {
-        address owner;
-        string name;
-        string creator;
-        string uri;
-        uint256 tokenId;
-    }
-
-    mapping(uint256 => NFT) public AuctionCollection;
 
     function registerNFT(
         address owner,
         string memory name,
         string memory creator,
         string memory uri
-    ) public payable onlyOwner {
+    ) public payable onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
-
         _tokenIdCounter.increment();
         _safeMint(owner, tokenId);
         _setTokenURI(tokenId, uri);
 
         require(msg.value == registrationPrice, "Price must be equal to listing price of 0.01 ETH");
         payable(msg.sender);
-
     }
 
     // The following functions are overrides required by Solidity.
@@ -93,5 +83,14 @@ contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable, MaticUsdMumbaiOra
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
