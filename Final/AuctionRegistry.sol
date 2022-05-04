286ec1176ef3a9a4e6f1e801e57b54fcabfe905c
tyedem <90783116+tyedem@users.noreply.github.com>
2022-05-04 18:13:37 -0400

    Add detailed comments to code

--- Final/AuctionRegistry.sol
+++ Final/AuctionRegistry.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-
+//Create contract for ChainLink MATIC/USD priceFeed
 contract MaticUsdMumbaiOracle {
 
     AggregatorV3Interface internal priceFeed;
@@ -41,15 +41,24 @@ contract MaticUsdMumbaiOracle {
     }   
 }
 
+// Inherit OpenZeppelin and ChainLink contracts
+// OpenZeppelin ERC721 is standards contract
+// OpenZeppelin ERC721URIStorage is a URI storage contract
+// OpenZeppelin Ownable is an access control contract which limits functionality to OnlyOwner
+// ChainLink MaticUsdMumbaiOracle contract is used for MATIC/USD priceFeed data
 contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable, MaticUsdMumbaiOracle {
+
+    //Calling Counters contract function for _tokenIdCounter
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
     address contractAddress;
 
+// Create contructor to link this contract with the auction contract
     constructor(address auctionAddress) ERC721("AuctionRegistry", "AUT") {
         contractAddress = auctionAddress;
     }
 
+// Define NFT data structure
     struct NFT {
         address owner;
         string name;
@@ -57,22 +66,33 @@ contract AuctionRegistry is ERC721, ERC721URIStorage, Ownable, MaticUsdMumbaiOra
         string uri;
     }
 
+// Map AuctionCollection for NFT details
     mapping(uint256 => NFT) public AuctionCollection;
 
+// Create registerNFT contract where only contract deployer may register NFTs
     function registerNFT(
         address owner,
         string memory name,
         string memory creator,
         string memory uri
     ) public onlyOwner returns (uint256) {
+
+        // Use Counters to track tokenIDs
         uint256 tokenId = _tokenIdCounter.current();
 
+        // Increment token IDs
         _tokenIdCounter.increment();
+
+        // Mint NFT
         _safeMint(owner, tokenId);
+
+        // Set token URI
         _setTokenURI(tokenId, uri);
-    
+
+        // Add tokenID to AuctionCollection
         AuctionCollection[tokenId] = NFT(owner, name, creator, uri);
 
+        // Return registered NFT tokenId
         return tokenId;
 
     }
