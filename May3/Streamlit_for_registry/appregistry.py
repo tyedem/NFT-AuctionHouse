634297720cdd2a5811fb3bf9f6a5a126272ee9ce
tyedem <90783116+tyedem@users.noreply.github.com>
2022-05-04 15:13:46 -0400

    Add Pinata functionality for IPFS Hashing

--- May3/Streamlit_for_registry/appregistry.py
+++ May3/Streamlit_for_registry/appregistry.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
 
+from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
+
 load_dotenv()
 
 # Define and connect a new Web3 provider
@@ -40,22 +42,54 @@ def load_contract():
 contract = load_contract()
 
 
-st.title("Registry System for NFT Auction Marketplace")
+################################################################################
+# Helper functions to pin files and json to Pinata
+################################################################################
+
+
+def pin_artwork(artwork_name, artwork_file):
+    # Pin the file to IPFS with Pinata
+    ipfs_file_hash = pin_file_to_ipfs(artwork_file.getvalue())
+
+    # Build a token metadata file for the artwork
+    token_json = {
+        "name": artwork_name,
+        "image": ipfs_file_hash
+    }
+    json_data = convert_data_to_json(token_json)
+
+    # Pin the json to IPFS with Pinata
+    json_ipfs_hash = pin_json_to_ipfs(json_data)
+
+    return json_ipfs_hash
+
+
+def pin_appraisal_report(report_content):
+    json_report = convert_data_to_json(report_content)
+    report_ipfs_hash = pin_json_to_ipfs(json_report)
+    return report_ipfs_hash
+
+
+st.title("T-GRAM NFT Auction House Registry System")
 st.write("Choose an account to get started")
 accounts = w3.eth.accounts
 address = st.selectbox("Select Account", options=accounts)
-st.markdown("---")
 
 ################################################################################
 # Register New Artwork for NFT Auction
 ################################################################################
 st.markdown("## Register New NFT")
-
-artwork_name = st.text_input("Enter the name of the nft")
-artist_name = st.text_input("Enter the artist name")
-artwork_uri = st.text_input("Enter the URI to the nft")
-
-if st.button("Register Artwork"):
+artwork_name = st.text_input("Enter the name of the NFT")
+artist_name = st.text_input("Enter the creator name")
+file = st.file_uploader("Upload NFT", type=["jpg", "jpeg", "png"])
+if st.button("Register NFT"):
+    artwork_ipfs_hash = pin_artwork(artwork_name, file)
+    artwork_uri = f"ipfs://{artwork_ipfs_hash}"
+    print(address,
+        artwork_name,
+        artist_name,
+        artwork_uri
+        )
     tx_hash = contract.functions.registerNFT(
         address,
         artwork_name,
@@ -65,5 +99,6 @@ if st.button("Register Artwork"):
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
+    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
+    st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{artwork_ipfs_hash})")
 st.markdown("---")
-
